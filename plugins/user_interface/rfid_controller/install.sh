a9f03886ed0260a40c2fe760a58b9bab5b5a54d1
Matthias Feist <matf.de@gmail.com>
2019-05-02 19:06:32 +0000

    added install steps to the install.sh file

--- plugins/user_interface/rfid_controller/install.sh
+++ plugins/user_interface/rfid_controller/install.sh
@@ -2,12 +2,24 @@
 
 echo "Installing rfid controller Dependencies"
 sudo apt-get update
-# Install the required packages via apt-get
-sudo apt-get -y install
+# required because we need "make"
+sudo apt-get -y install build-essential
 
-# If you need to differentiate install for armhf and i386 you can get the variable like this
-#DPKG_ARCH=`dpkg --print-architecture`
-# Then use it to differentiate your install
+# prepare drivers
+cd node_modules/wiringpi-node/
+sudo ./install.sh
+
+# enable driver
+grep -qxF 'dtparam=spi=on' /boot/config.txt || echo 'dtparam=spi=on' >> /boot/config.txt
+
+echo " "
+echo "----------------------------------------"
+echo " "
+echo "              IMPORTANT!"
+echo " You have to restart your Raspberry PI"
+echo " "
+echo "----------------------------------------"
+echo " "
 
 #requred to end the plugin install
 echo "plugininstallend"
